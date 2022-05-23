61b5d1d0fe54f901edb7d572122da6e12dee12a7
Badar Wildani <wildanie12@gmail.com>
2022-05-23 23:03:58 +0700

    add: database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Answer;
+use App\Models\AnswerItem;
+use App\Models\Form;
+use App\Models\FormPages;
+use App\Models\Question;
+use App\Models\QuestionItem;
+use Database\Factories\AnswerItemFactory;
+use Database\Factories\QuestionFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,11 +22,50 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // if (
+        //     $answerType == 'DROPDOWN' ||
+        //     $answerType == 'CHECKBOX' ||
+        //     $answerType == 'RADIO' ||
+        //     $answerType == 'RADIO_CUSTOM_TEXT' ||
+        //     $answerType == 'CHECKBOX_CUSTOM_TEXT'
+        // ) {
+        //     // Add Answer Items
+        //     $answer);
+        // }
+        for ($i = 0; $i < 2; $i++) {
+            // Answer
+            $answer = Answer::factory()
+                ->has(
+                    AnswerItem::factory()
+                        ->count(rand(2, 4))
+                        ->state(fn ($definition) => [
+                            'is_custom_input_trigger' => [true, false][rand(0, 1)]
+                        ])
+                );
+            $answerType = $answer->definition()['type'];
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+
+            // Form Pages
+            $formPages = FormPages::factory()
+                ->count(rand(1, 3))
+                ->sequence(fn ($sequence) => [ 'order' => $sequence->index+1 ])
+                ->has(
+                    Question::factory()
+                        ->sequence(fn ($sequence) => [
+                            'order' => $sequence->index+1
+                        ])
+                        ->for($answer, 'answer')
+                        ->has(
+                            QuestionItem::factory()
+                                ->count(rand(0, 5))
+                                ->sequence(fn ($sequence) => [ 'order' => $sequence->index+1 ])
+                        )
+                );
+
+            // Form
+            $form = Form::factory()
+                ->has($formPages, 'formPages')
+                ->create();
+        }
     }
 }
