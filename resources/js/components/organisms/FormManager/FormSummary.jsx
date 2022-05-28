f5d4519282125d81290886b99a8b3dbde3907d07
Badar Wildani <wildanie12@gmail.com>
2022-05-29 01:27:32 +0700

    fix: summary card to the right

--- resources/js/components/organisms/FormManager/FormSummary.jsx
+++ resources/js/components/organisms/FormManager/FormSummary.jsx
@@ -16,24 +16,22 @@ const FormSummary = () => {
         <div className="relative flex items-center w-full h-56">
             <HeaderVector className="absolute bottom-0 left-0 z-0 w-auto h-full" />
             <div className="z-10 flex items-center w-full font-poppins">
-                <div className="flex flex-grow py-8">
-                    <div className="flex items-center ml-16 md:w-1/2 ">
-                        <div className="flex flex-col items-start justify-start">
-                            <h2 className="mr-10 text-2xl font-bold">
-                                Your Forms
-                            </h2>
-                            <div className="text-lg text-gray-500">
-                                List of all your active forms
-                            </div>
-                            <Link className="w-auto mt-2 btn btn-success">
-                                <FontAwesomeIcon
-                                    icon={faPlus}
-                                    className="w-4 aspect-square"
-                                />
-                                <div>Create Form</div>
-                            </Link>
+                <div className="flex items-center ml-16 md:w-1/2 ">
+                    <div className="flex flex-col items-start justify-start">
+                        <h2 className="mr-10 text-2xl font-bold">Your Forms</h2>
+                        <div className="text-lg text-gray-500">
+                            List of all your active forms
                         </div>
+                        <Link className="w-auto mt-2 btn btn-success">
+                            <FontAwesomeIcon
+                                icon={faPlus}
+                                className="w-4 aspect-square"
+                            />
+                            <div>Create Form</div>
+                        </Link>
                     </div>
+                </div>
+                <div className="flex justify-between flex-grow">
                     <div className="flex-col items-center hidden space-y-2 md:flex md:w-auto md:items-stretch">
                         <SummaryCard
                             Icon={
@@ -58,8 +56,8 @@ const FormSummary = () => {
                             className="text-primary"
                         />
                     </div>
+                    <HeaderIllustration className="relative z-[1] w-56 h-52" />
                 </div>
-                <HeaderIllustration className="relative z-[1] w-56 h-52" />
                 <DotGrid className="absolute right-0 rotate-90 w-28 aspect-square z-0 translate-x-[32%] top-0" />
             </div>
         </div>
