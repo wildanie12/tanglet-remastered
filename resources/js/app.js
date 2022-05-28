7bed49cd664f412f9e2db10206039e4b68b15f2a
Badar Wildani <wildanie12@gmail.com>
2022-05-28 08:01:50 +0700

    add: reactjs laravel ui implementation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
-require('./bootstrap');
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes React and other helpers. It's a great starting point while
+ * building robust, powerful web applications using React + Laravel.
+ */
+
+require("./bootstrap");
+
+/**
+ * Next, we will create a fresh React component instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+import React from "react";
+import { render } from "react-dom";
+import { createInertiaApp } from "@inertiajs/inertia-react";
+
+createInertiaApp({
+    resolve: (name) => require(`./pages/${name}`),
+    setup({ el, App, props }) {
+        render(<App {...props} />, el);
+    },
+});
---
+++ resources/js/pages/User/FormManager.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FormManager = () => {
+    return <div className="bg">FormManager</div>;
+};
+
+export default FormManager;
