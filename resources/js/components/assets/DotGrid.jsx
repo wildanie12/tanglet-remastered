70ad27847fe77dc62c48a13ff663dd8a52d97fe2
Badar Wildani <wildanie12@gmail.com>
2022-05-29 00:28:53 +0700

    add: form summary assets illustration

---
+++ resources/js/components/assets/DotGrid.jsx
@@ -0,0 +1,129 @@
+import React from "react";
+
+const DotGrid = ({ ...props }) => {
+    return (
+        <svg
+            width="100"
+            height="100"
+            viewBox="0 0 100 100"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        >
+            <circle
+                cx="50.3643"
+                cy="6.29553"
+                r="4.45161"
+                transform="rotate(45 50.3643 6.29553)"
+                fill="#FFAC00"
+            />
+            <circle
+                cx="64.5293"
+                cy="20.4604"
+                r="4.45161"
+                transform="rotate(45 64.5293 20.4604)"
+                fill="#FFAC00"
+            />
+            <circle
+                cx="78.6941"
+                cy="34.6254"
+                r="4.45161"
+                transform="rotate(45 78.6941 34.6254)"
+                fill="#FFAC00"
+            />
+            <circle
+                cx="92.8591"
+                cy="48.7904"
+                r="4.45161"
+                transform="rotate(45 92.8591 48.7904)"
+                fill="#FFAC00"
+            />
+            <circle
+                cx="6.29565"
+                cy="50.3643"
+                r="4.45161"
+                transform="rotate(45 6.29565 50.3643)"
+                fill="#FFAC00"
+            />
+            <circle
+                cx="20.4604"
+                cy="64.5292"
+                r="4.45161"
+                transform="rotate(45 20.4604 64.5292)"
+                fill="#FFAC00"
+            />
+            <circle
+                cx="34.6255"
+                cy="78.6942"
+                r="4.45161"
+                transform="rotate(45 34.6255 78.6942)"
+                fill="#FFAC00"
+            />
+            <circle
+                cx="48.7903"
+                cy="92.8591"
+                r="4.45161"
+                transform="rotate(45 48.7903 92.8591)"
+                fill="#FFAC00"
+            />
+            <circle
+                cx="35.4124"
+                cy="21.2474"
+                r="4.45161"
+                transform="rotate(45 35.4124 21.2474)"
+                fill="#FFAC00"
+            />
+            <circle
+                cx="49.5774"
+                cy="35.4124"
+                r="4.45161"
+                transform="rotate(45 49.5774 35.4124)"
+                fill="#FFAC00"
+            />
+            <circle
+                cx="63.7422"
+                cy="49.5773"
+                r="4.45161"
+                transform="rotate(45 63.7422 49.5773)"
+                fill="#FFAC00"
+            />
+            <circle
+                cx="77.9072"
+                cy="63.7422"
+                r="4.45161"
+                transform="rotate(45 77.9072 63.7422)"
+                fill="#FFAC00"
+            />
+            <circle
+                cx="21.2473"
+                cy="35.4124"
+                r="4.45161"
+                transform="rotate(45 21.2473 35.4124)"
+                fill="#FFAC00"
+            />
+            <circle
+                cx="35.4124"
+                cy="49.5773"
+                r="4.45161"
+                transform="rotate(45 35.4124 49.5773)"
+                fill="#FFAC00"
+            />
+            <circle
+                cx="49.5774"
+                cy="63.7422"
+                r="4.45161"
+                transform="rotate(45 49.5774 63.7422)"
+                fill="#FFAC00"
+            />
+            <circle
+                cx="63.7422"
+                cy="77.9072"
+                r="4.45161"
+                transform="rotate(45 63.7422 77.9072)"
+                fill="#FFAC00"
+            />
+        </svg>
+    );
+};
+
+export default DotGrid;
---
+++ resources/js/components/assets/form-manager/HeaderVector.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const HeaderVector = ({ ...props }) => {
+    return (
+        <svg
+            width="266"
+            height="225"
+            viewBox="0 0 266 225"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        >
+            <path
+                d="M16.5 97.5C11.7 66.3 7.16667 10.1667 0 0V224.5H265.5C249 221.333 204.9 214.8 160.5 214C105 213 90 180.481 64.5 175C21.7 165.8 19.3333 124.667 16.5 97.5Z"
+                fill="#1C425A"
+            />
+        </svg>
+    );
+};
+
+export default HeaderVector;
