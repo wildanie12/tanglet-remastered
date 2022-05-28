7af4de94b253cc77e28bca1cd5e1c3a6206e637c
Badar Wildani <wildanie12@gmail.com>
2022-05-29 01:24:46 +0700

    add: input elements to filter

---
+++ resources/js/components/organisms/FormManager/FormFilter.jsx
@@ -0,0 +1,79 @@
+import {
+    faCalendarAlt,
+    faLightbulb,
+    faSearch,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import InputSelect from "../../atoms/forms/InputSelect";
+
+import InputTextLabel from "../../atoms/forms/InputTextLabel";
+
+const FormFilter = ({ className }) => {
+    return (
+        <div className={`p-6 text-white bg-primary ${className}`}>
+            <div className="flex items-center w-full mx-auto space-x-3 xl:w-10/12">
+                <div className="lg:w-4/12">
+                    <InputTextLabel
+                        name="filter_search"
+                        label="Search by Title"
+                        placeholder="Search form title here ..."
+                        Icon={
+                            <FontAwesomeIcon
+                                icon={faSearch}
+                                className="w-4 h-4 text-gray-500"
+                            />
+                        }
+                    />
+                </div>
+                <div className="lg:w-2/12">
+                    <InputTextLabel
+                        name="filter_date_from"
+                        label="From Date"
+                        placeholder="Write here ..."
+                        type="date"
+                        Icon={
+                            <FontAwesomeIcon
+                                icon={faCalendarAlt}
+                                className="w-4 h-4 text-gray-500"
+                            />
+                        }
+                    />
+                </div>
+                <div className="lg:w-2/12">
+                    <InputTextLabel
+                        name="filter_date_to"
+                        label="To Date"
+                        type="date"
+                        placeholder="Write here ..."
+                        Icon={
+                            <FontAwesomeIcon
+                                icon={faCalendarAlt}
+                                className="w-4 h-4 text-gray-500"
+                            />
+                        }
+                    />
+                </div>
+                <div className="lg:w-2/12">
+                    <InputSelect
+                        name="filter_status"
+                        label="Status"
+                        Icon={
+                            <FontAwesomeIcon
+                                icon={faLightbulb}
+                                className="w-4 h-4 text-gray-500"
+                            />
+                        }
+                        options={[
+                            { value: "", text: "All" },
+                            { value: "LOCKED", text: "Locked" },
+                            { value: "UNLOCKED", text: "Unlocked" },
+                        ]}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FormFilter;
