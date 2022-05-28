f772d947ccccf89c213d0020f1ca2e7b39dbddfd
Badar Wildani <wildanie12@gmail.com>
2022-05-28 11:50:38 +0700

    add: configure tailwindcss

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,5 @@ const mix = require("laravel-mix");
  */
 
 mix.js("resources/js/app.js", "public/js")
-    .react()
-    .postCss("resources/css/app.css", "public/css", [require("tailwindcss")]);
+    .postCss("resources/css/app.css", "public/css", [require("tailwindcss")])
+    .react();
