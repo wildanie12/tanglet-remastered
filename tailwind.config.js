ee9282f92ba9ba166b4c845e95fbc26f0d4b9b8c
Badar Wildani <wildanie12@gmail.com>
2022-05-28 11:51:02 +0700

    add: color theme font definition

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,26 @@
 module.exports = {
-    content: [],
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.jsx",
+        "./resources/**/*.vue",
+    ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "#1C425A",
+                secondary: "#FFAC00",
+                accent: "#BF0404",
+                neutral: "#3D4451",
+                info: "#6BB3FF",
+                success: "#00947B",
+                warning: "#FF9900",
+                error: "#FF2424",
+            },
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"],
+            },
+        },
     },
     plugins: [],
 };
