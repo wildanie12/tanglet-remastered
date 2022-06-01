3bc0e2d00d82b7bb08cc57193584f50ece5b9217
Badar Wildani <wildanie12@gmail.com>
2022-06-02 01:18:01 +0700

    fix: added padding bottom for card shadow

--- resources/js/components/organisms/FormManager/FormCardList.jsx
+++ resources/js/components/organisms/FormManager/FormCardList.jsx
@@ -3,7 +3,7 @@ import FormCard from "../../molecules/cards/FormCard";
 
 const FormCardList = () => {
     return (
-        <div className="relative z-10 flex flex-wrap w-full px-4 -mt-24 md:px-8">
+        <div className="relative z-10 flex flex-wrap w-full px-4 pb-16 -mt-24 md:px-8">
             <div className="w-full p-2 md:w-1/2 lg:w-4/12">
                 <FormCard className="box-border" />
             </div>
